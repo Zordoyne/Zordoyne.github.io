e05acc925c5fe84f661f8afe32df88ed3a7a4024
Zachary Ordoyne <ordoyne2006@gmail.com>
2023-10-23 16:25:47 +0000

    partfolia start

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,10 +43,10 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+    createPlatform(200, 700, 100, 5);
+    createPlatform(200, 600, 150, 20)
+createPlatform(200,)
 
-
-
-    
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
